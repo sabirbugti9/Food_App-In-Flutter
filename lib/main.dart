f05c0204104547a7cdafbb82f8056165d3c26014
SabirBugti <myfirebase75@gmail.com>
2020-10-20 21:21:30 +0500

    Profile Screen Done

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
       //     }
       //   },
       // ),
-      home: SignUp(),
+      home: ProfileScreen(),
     );
   }
 }
--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,8 +1,138 @@
 import 'package:flutter/material.dart';
 import 'package:myfood_app/screens/homepage.dart';
 import 'package:myfood_app/widget/mybutton.dart';
+import 'package:myfood_app/widget/mypasswordtextformfield.dart';
+import 'package:myfood_app/widget/mytextformfield.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
+  @override
+  _ProfileScreenState createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  bool isEdit = false;
+  bool isMale = false;
+  final TextEditingController email = TextEditingController();
+  final TextEditingController fullName = TextEditingController();
+  final TextEditingController phoneNumber = TextEditingController();
+  final TextEditingController address = TextEditingController();
+  final TextEditingController password = TextEditingController();
+  static String p =
+      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+  static RegExp regExp = new RegExp(p);
+  void vaildation() {
+    if (email.text.isEmpty &&
+        password.text.isEmpty &&
+        fullName.text.isEmpty &&
+        address.text.isEmpty &&
+        phoneNumber.text.isEmpty) {
+      scaffold.currentState.showSnackBar(
+        SnackBar(
+          content: Text("All Fleid Is Empty"),
+        ),
+      );
+    } else if (fullName.text.isEmpty) {
+      scaffold.currentState.showSnackBar(
+        SnackBar(
+          content: Text("FullName Is Empty"),
+        ),
+      );
+    } else if (email.text.isEmpty) {
+      scaffold.currentState.showSnackBar(
+        SnackBar(
+          content: Text("Email Is Empty"),
+        ),
+      );
+    } else if (!regExp.hasMatch(email.text)) {
+      scaffold.currentState.showSnackBar(
+        SnackBar(
+          content: Text("Email Is Not Vaild"),
+        ),
+      );
+    } else if (phoneNumber.text.isEmpty) {
+      scaffold.currentState.showSnackBar(
+        SnackBar(
+          content: Text("Phone Number Is Empty"),
+        ),
+      );
+    } else if (phoneNumber.text.length < 11) {
+      scaffold.currentState.showSnackBar(
+        SnackBar(
+          content: Text("Phone Number Must Be 11 "),
+        ),
+      );
+    } else if (password.text.isEmpty) {
+      scaffold.currentState.showSnackBar(
+        SnackBar(
+          content: Text("Password Is Empty"),
+        ),
+      );
+    } else if (password.text.length < 8) {
+      scaffold.currentState.showSnackBar(
+        SnackBar(
+          content: Text("Password Is Too Short"),
+        ),
+      );
+    } else {}
+  }
+
+  final GlobalKey<ScaffoldState> scaffold = GlobalKey<ScaffoldState>();
+  Widget _buildAllTextFormField() {
+    return Container(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          MyTextFormField(
+            controller: fullName,
+            title: "FullName",
+          ),
+          MyTextFormField(
+            controller: email,
+            title: "Email",
+          ),
+          MyTextFormField(
+            controller: phoneNumber,
+            title: "Phone Number",
+          ),
+          MyTextFormField(
+            controller: address,
+            title: "Address",
+          ),
+          GestureDetector(
+            onTap: () {
+              setState(() {
+                isMale = !isMale;
+              });
+            },
+            child: Container(
+              height: 60,
+              width: double.infinity,
+              padding: EdgeInsets.only(left: 10),
+              alignment: Alignment.centerLeft,
+              child: Text(
+                isMale == false ? "Female" : "Male",
+                style: TextStyle(fontSize: 16, color: Colors.grey),
+              ),
+              decoration: BoxDecoration(
+                  color: Color(0xfff5d8e4),
+                  borderRadius: BorderRadius.circular(10)),
+            ),
+          ),
+          MyPasswordTextFormField(
+            controller: password,
+            title: "Password",
+          ),
+          MyButton(
+            name: "Update",
+            onPressed: () {
+              vaildation();
+            },
+          ),
+        ],
+      ),
+    );
+  }
+
   Widget _buildSingleContainer({String name}) {
     return Container(
       height: 60,
@@ -25,6 +155,7 @@ class ProfileScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: scaffold,
       appBar: AppBar(
         elevation: 0.0,
         leading: IconButton(
@@ -54,12 +185,19 @@ class ProfileScreen extends StatelessWidget {
         actions: [
           Padding(
             padding: const EdgeInsets.only(top: 15, right: 10),
-            child: Text(
-              "Edit",
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.white,
-                fontWeight: FontWeight.w900,
+            child: GestureDetector(
+              onTap: () {
+                setState(() {
+                  isEdit = !isEdit;
+                });
+              },
+              child: Text(
+                isEdit == false ? "Edit" : "Close",
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                  fontWeight: FontWeight.w900,
+                ),
               ),
             ),
           ),
@@ -96,20 +234,20 @@ class ProfileScreen extends StatelessWidget {
                   child: Container(
                     padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
                     color: Colors.white,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
-                        _buildSingleContainer(name: "Sabir Bugti"),
-                        _buildSingleContainer(name: "Sabirbugti@gmail.com"),
-                        _buildSingleContainer(name: "12345678910"),
-                        _buildSingleContainer(name: "Male"),
-                        _buildSingleContainer(name: "Balochistan,Pakistan"),
-                        MyButton(
-                          name: "Update",
-                          onPressed: () {},
-                        )
-                      ],
-                    ),
+                    child: isEdit == false
+                        ? Column(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              _buildSingleContainer(name: "Sabir Bugti"),
+                              _buildSingleContainer(
+                                  name: "Sabirbugti@gmail.com"),
+                              _buildSingleContainer(name: "12345678910"),
+                              _buildSingleContainer(name: "Male"),
+                              _buildSingleContainer(
+                                  name: "Balochistan,Pakistan"),
+                            ],
+                          )
+                        : _buildAllTextFormField(),
                   ),
                 ),
               ],
